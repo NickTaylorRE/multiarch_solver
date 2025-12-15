550a72f57144ced809594f3055e72f6d2d0af05e
pjun <NickTaylorRE@protonmail.com>
2025-12-14 19:24:03 -0600

    symbex

---
+++ src/symbexvm.rs
@@ -0,0 +1,13 @@
+// this is a symbolic exectuion engine for the VM
+
+// the original VM had
+// context 
+//     stack
+//     registers
+// math
+//
+// we need an abstraction that can hold a value or a symvar
+// this also meens we need a way represent a symvar
+// then we will need math functions that can handle a symvar or values
+//
+// 
